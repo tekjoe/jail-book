dd047607d2c8771f696cfa4872be5802ad0d7363
tekjoe <joe@tekjoe.org>
2025-02-27 13:39:24 -0600

    fixes params should be awaited errors

--- src/app/(main)/county/[county]/page.tsx
+++ src/app/(main)/county/[county]/page.tsx
@@ -4,29 +4,30 @@ import InmateTable from '@/components/InmateTable';
 import { getInmatesByCountyPaginated, getCountyByName, WISCONSIN_COUNTIES } from '@/lib/supabase';
 
 interface CountyPageProps {
-  params: {
+  params: Promise<{
     county: string;
-  };
+  }>;
 }
 
-export default async function CountyPage({ params }: CountyPageProps) {
+export default async function CountyPage(props: CountyPageProps) {
+  const params = await props.params;
   // Format the county name for database lookup
   const { county: countyParam } = params;
   const formattedCounty = countyParam
     .split('-')
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
-  
+
   // Get county from database
   const county = await getCountyByName(formattedCounty);
-  
+
   if (!county) {
     notFound();
   }
-  
+
   // Fetch initial inmates for this county
   const { inmates, totalCount } = await getInmatesByCountyPaginated(county.name, 1, 25);
-  
+
   return (
     <div className="w-full">
       <InmateTable 
